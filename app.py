54cb0c397adfcd896963dcc8feb9e153e87033b6
tokyliu623 <469753809@qq.com>
2025-05-30 11:34:06 +0800

    port update

--- app.py
+++ app.py
@@ -784,4 +784,4 @@ def process_cover(cover_url, api_key, index, session_id):
             time.sleep(2)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
